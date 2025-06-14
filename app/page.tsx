0e44477d1b62e764d4d588e92f1a3b7c6ea80559
TheRaulH <kevinlopezc05@gmail.com>
2025-06-14 10:58:44 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import ExerciseList from "@/components/organisms/ExerciseList";
+
+export default function Home() {
+  return (
+    <div className="  min-h-screen text-white p-4 w-96 mx-auto ">
+      <ExerciseList />
+    </div>
+  );
+}
